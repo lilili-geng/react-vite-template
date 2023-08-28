4ad1c9652b1f5216d1cbdc741bab9880b0fd376e
lilili-geng <1229364040@qq.com>
2023-08-28 20:43:34 +0800

    fix: upload

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,28 @@ import { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 // import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { RenderRouters } from './router';
+// import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+// import { Web3Modal } from '@web3modal/react'
+// import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+// import { arbitrum, mainnet, polygon } from 'wagmi/chains'
 import {
   RecoilRoot,
 } from 'recoil';
+
+// const chains = [arbitrum, mainnet, polygon]
+// const projectId = '407604896f292eb30f1c6e8df2e3d620'
+
+// const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
+// const wagmiConfig = createConfig({
+//   autoConnect: true,
+//   connectors: w3mConnectors({ projectId, chains }),
+//   publicClient
+// })
+// const ethereumClient = new EthereumClient(wagmiConfig, chains)
+
 export default function App() {
   return (
+    // <WagmiConfig config={wagmiConfig}>
     <RecoilRoot>
       {/* <RainbowKitProvider chains={chains}> */}
       <BrowserRouter>
@@ -16,5 +33,7 @@ export default function App() {
       </BrowserRouter>
       {/* </RainbowKitProvider> */}
     </RecoilRoot>
+    //   <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+    // </WagmiConfig>
   );
 }
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,8 +1,10 @@
 import { useRecoilState } from "recoil";
 import Store from "@/store";
 import { Row, Col, Button } from "antd";
+import { useWeb3Modal } from '@web3modal/react'
 
 export default function Home() {
+  const { open, close } = useWeb3Modal()
   const [nameState, setNameState] = useRecoilState(Store.nameState);
 
   return (
@@ -12,6 +14,7 @@ export default function Home() {
         <Button onClick={() => { setNameState("王五"); }}>
           点击
         </Button>
+        <button onClick={() => open()}>Connect</button>
       </Col>
     </Row>
   )
